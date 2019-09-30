ecbe94baaf1efa76d83a8e1091d10ea179c2d9c2
CristianTokyo <qmc.csanchez@gmail.com>
2019-09-30 17:08:23 -0500

    database

--- database/events.sql
+++ database/events.sql
@@ -30,8 +30,9 @@ CREATE TABLE IF NOT EXISTS `events` (
   `ename` VARCHAR(100) NOT NULL,
   `manager` VARCHAR(100) NULL,
   `mcharge` VARCHAR(100) NULL,
-  `hr` TIME NOT NULL,
-  `day` INT(1) NOT NULL,
+  `beginhr` TIME NOT NULL,
+  `finishhr` TIME NOT NULL,
+  `day` INT(2) NOT NULL,
   `size` INT NOT NULL,
   CONSTRAINT `fk_types` 
     FOREIGN KEY (`idtype`) REFERENCES `types`(`idtype`),
@@ -50,10 +51,26 @@ CREATE TABLE IF NOT EXISTS `users_events` (
   CONSTRAINT `fk_users_events_events1`
     FOREIGN KEY (`idevent`) REFERENCES `events`(`idevent`));
 
-INSERT INTO `types` VALUES (null, 'Conferencia');
-INSERT INTO `events` VALUES (null, 1, 'Importancia del Factor Humano en El Sector Automotriz', 'Cristian Sanchez Velasco', 'CEO', '17:00', 0, 30);
-INSERT INTO `users` VALUES (null, 'cris@gmail.com', '1234');
-INSERT INTO `users_events` VALUES (null, 1, 1);
+
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- INSERT INTO `types` VALUES (null, 'Conferencia');
+-- INSERT INTO `events` VALUES (null, 1, 'Importancia del Factor Humano en El Sector Automotriz', 'Cristian Sanchez Velasco', 'CEO', '17:00', 0, 30);
+-- INSERT INTO `users` VALUES (null, 'cris@gmail.com', '1234');
+-- INSERT INTO `users_events` VALUES (null, 1, 1);
+
+INSERT INTO `types` (`idtype`, `type`) VALUES (null, 'Conferencia Magistral');
+INSERT INTO `types` (`idtype`, `type`) VALUES (null, 'Panel Magistral');
+INSERT INTO `types` (`idtype`, `type`) VALUES (null, 'Recorrido');
+INSERT INTO `types` (`idtype`, `type`) VALUES (null, 'Concurso');
+INSERT INTO `types` (`idtype`, `type`) VALUES (null, 'Conferencia');
+INSERT INTO `types` (`idtype`, `type`) VALUES (null, 'Taller');
+INSERT INTO `types` (`idtype`, `type`) VALUES (null, 'Exposición');
+
+
+
+INSERT INTO `events` (`idtype`, `ename`, `manager`, `mcharge`, `beginhr`, `finishhr`, `day`, `size`) VALUES ( 1,"Tendencias de la Industria Automotriz","Ing. Armando Ávila Moreno","Vicepresidente de Manufactura de Nissan Mexicana","10:00","11:00",26,100);
+
 
 
 select * from users_events inner join users using(idusr) inner join events using(idevent) inner join types using(idtype); 
