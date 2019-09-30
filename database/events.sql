422aac7b470cddbfdd4d80103f4a8bbb077def44
CristianTokyo <qmc.csanchez@gmail.com>
2019-09-30 15:48:43 -0500

    database

---
+++ database/events.sql
@@ -0,0 +1,59 @@
+
+DROP SCHEMA IF EXISTS `events`;
+
+CREATE SCHEMA IF NOT EXISTS `events` DEFAULT CHARACTER SET utf8 ;
+USE `events` ;
+
+-- -----------------------------------------------------
+-- USERS
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `users` (
+  `idusr` INT NOT NULL AUTO_INCREMENT,
+  `email` VARCHAR(100) NOT NULL,
+  `pass` VARCHAR(100) NOT NULL,
+  PRIMARY KEY (`idusr`));
+
+-- -----------------------------------------------------
+-- TYPES (events)
+-- -----------------------------------------------------
+  CREATE TABLE IF NOT EXISTS `types` (
+  `idtype` INT NOT NULL AUTO_INCREMENT,
+  `type` VARCHAR(100) NOT NULL,
+  PRIMARY KEY (`idtype`));
+
+-- -----------------------------------------------------
+-- EVENTS
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `events` (
+  `idevent` INT NOT NULL AUTO_INCREMENT,
+  `idtype` INT NULL,
+  `ename` VARCHAR(100) NOT NULL,
+  `manager` VARCHAR(100) NULL,
+  `mcharge` VARCHAR(100) NULL,
+  `hr` TIME NOT NULL,
+  `day` INT(1) NOT NULL,
+  `size` INT NOT NULL,
+  CONSTRAINT `fk_types` 
+    FOREIGN KEY (`idtype`) REFERENCES `types`(`idtype`),
+  PRIMARY KEY (`idevent`));
+
+-- -----------------------------------------------------
+-- USERS_EVENTS
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `users_events` (
+  `idusrevent` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  `idusr` INT NOT NULL,
+  `idevent` INT NOT NULL,
+  PRIMARY KEY (`idusrevent`),
+  CONSTRAINT `fk_users_events_users` 
+    FOREIGN KEY (`idusr`) REFERENCES `users`(`idusr`),
+  CONSTRAINT `fk_users_events_events1`
+    FOREIGN KEY (`idevent`) REFERENCES `events`(`idevent`));
+
+INSERT INTO `types` VALUES (null, 'Conferencia kk');
+INSERT INTO `events` VALUES (null, 1, 'caca', 'yo', 'gfe', '17:00', 0, 30);
+INSERT INTO `users` VALUES (null, 'cris@gmail.com', '1234');
+INSERT INTO `users_events` VALUES (null, 1, 1);
+
+
+select * from users_events inner join users using(idusr) inner join events using(idevent) inner join types using(idtype); 
