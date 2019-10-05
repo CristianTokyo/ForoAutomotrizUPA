c00dbdb696ad2f85178b568d2aca3c6b0e845bee
ISEIUPA <luis.ernesto.anaya@upa.edu.mx>
2019-10-04 23:35:45 -0500

    Arreglo a conexion y validacion

--- conexion.php
+++ conexion.php
@@ -1,7 +1,13 @@
 <?php
 try {
-    $conexion = new PDO ("mysql:host=localhost;dbname=events",'root','');
- 
+
+    $username = 'root';
+    $password = '';
+    $database = 'events';
+    $linkHost = 'localhost';
+    $conexion = mysqli_connect($linkHost, $username, $password) or die ("problemas de coneccion");
+    $baseUsuarios = mysqli_select_db($conexion, $database)or die("problemas de conexión DB");
+
 } catch (PDOException $e) {
     echo "Error de conexión ". $e->getMessage();
 }
--- validate.php
+++ validate.php
@@ -1,13 +1,17 @@
 <?php
 if (isset($_POST["button"])) {
     require_once("conexion.php");
-
     $email = $_POST['email'];
     $pass = $_POST['pass'];
-    $result = mysqli_query($conexion, "Select email, pass from users where email= $email;");
-    $row = mysqli_fetch_array($result, MYSQLI_BOTH);
+    $sql = "SELECT pass,email from users WHERE email = '$email'";
+    if ($result = mysqli_query($conexion, $sql)){
+      $row   = mysqli_fetch_row($result);    }
+    //$result = mysqli_query($conexion, "Select email from users where email= '$email';");
+    //$row = mysqli_fetch_array($result);
 
-    if ($pass != $row[0] && $email != $row[1]) echo "<script> alert('Verifique Los Datos'); </script>";
+
+    if ($pass != $row[0]|| $email != $row[1]) {
+      echo "<script> alert('Verifique Los Datos'); </script>";}
     else {
      header("location:formulario.php "); // poner a vista de formulario
     }
