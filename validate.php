77a3225b2e2ab293d12484e010bb0eba58508ba4
juan12356789 <up170132@alumnos.upa.edu.mx>
2019-10-06 14:28:53 -0500

    el bueno

--- validate.php
+++ validate.php
@@ -6,7 +6,7 @@ if (isset($_POST["button"])) {
     $pass = $_POST['pass'];
 
 
-    $sql = "SELECT pass,email from users WHERE email = '$email'";
+    $sql = "SELECT pass,email from users WHERE email = '$email' and pass = '$pass'";
     if ($result = mysqli_query($conexion, $sql)){
       $row   = mysqli_fetch_row($result);    }
     //$result = mysqli_query($conexion, "Select email from users where email= '$email';");
